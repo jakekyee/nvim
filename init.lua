bf47631a65deb2ff2bb19d71f5eeb904cced70cb
jakewsl <wsl@jakeyee.com>
2026-05-12 23:52:37 -0700

    fixed stuff

--- init.lua
+++ init.lua
@@ -3,6 +3,11 @@ vim.g.maplocalleader = " "
 
 local bin_path = vim.fn.stdpath("config") .. "/bin"
 vim.env.PATH = bin_path .. ":" .. vim.env.PATH
+
+
+--vim.opt.relativenumber = true
+vim.opt.number = true
+
 require("core.options")
 require("core.keymaps")
 require("core.autocmds")
